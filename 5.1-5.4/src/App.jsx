d0867081e6a25389b175a55cc037f3b66b0fd765
Wathuomichael <wathuomichael@gmail.com>
2024-09-06 19:23:58 +0300

    5.4 and logout

--- 5.1-5.4/src/App.jsx
+++ 5.1-5.4/src/App.jsx
@@ -3,6 +3,7 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import Login from './components/Login'
 import loginService from './services/users'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -10,6 +11,7 @@ const App = () => {
   const [blogTitle, setBlogTitle] = useState('')
   const [blogAuthor, setBlogAuthor] = useState('')
   const [blogUrl, setBlogUrl] = useState('')
+  const [message, setMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs => {
@@ -34,6 +36,10 @@ const App = () => {
       window.localStorage.setItem('loggedInUser', JSON.stringify(user))
     } catch (error) {
       console.log(error) 
+      setMessage({ content: 'Invalid Credentials', type: 'error' })
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
     }
   }
 
@@ -46,33 +52,47 @@ const App = () => {
       setBlogTitle('')
       setBlogAuthor('')
       setBlogUrl('')
+      setMessage({ content: `A new blog ${blogTitle} has been added`, type: 'info'})
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
     } catch (error) {
       console.log(error) 
     }
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedInUser')
+    setUser(null)
+  }
+
   return (
-    user === null ? 
-    <Login handleSubmit={handleUserSubmit}/> :
-      <div>
-        <form onSubmit={handleBlogSubmit}>
-          <h1>Create new</h1>
-          <div>
-            Title:<input type='text' value={blogTitle} onChange={({ target }) => setBlogTitle(target.value)}></input>
-          </div>
-          <div>
-            Author:<input type='text' value={blogAuthor} onChange={({ target }) => setBlogAuthor(target.value)}></input>
-          </div>
-          <div>
-            Url:<input type='text' value={blogUrl} onChange={({ target }) => setBlogUrl(target.value)}></input>
-          </div>
-          <button type='submit'>Create</button>
-        </form>
-        <h2>blogs</h2>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
-        )}
-      </div>
+    <div>
+      <Notification message={message}/>
+      {user === null ? 
+        <Login handleSubmit={handleUserSubmit}/> :
+        <div>
+          <h2>blogs</h2>
+          <p>{user.name} logged in</p>
+          <button onClick={handleLogout}>Logout</button>
+          <form onSubmit={handleBlogSubmit}>
+            <h1>Create new</h1>
+            <div>
+              Title:<input type='text' value={blogTitle} onChange={({ target }) => setBlogTitle(target.value)}></input>
+            </div>
+            <div>
+              Author:<input type='text' value={blogAuthor} onChange={({ target }) => setBlogAuthor(target.value)}></input>
+            </div>
+            <div>
+              Url:<input type='text' value={blogUrl} onChange={({ target }) => setBlogUrl(target.value)}></input>
+            </div>
+            <button type='submit'>Create</button>
+          </form>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>}
+    </div>
   )
 }
 
